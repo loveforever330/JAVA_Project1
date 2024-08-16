7ac61c1539070d8174b87e1e5a8073e495f31af9
GENCO <zhangjinke@bjfu.edu.cn>
2024-08-16 21:54:23 +0800

    add function to send mail

---
+++ community/src/test/java/com/nowcoder/community/LoggerTests.java
@@ -0,0 +1,23 @@
+package com.nowcoder.community;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.logging.Logger;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTests {
+
+    private static final Logger logger = (Logger) LoggerFactory.getLogger(LoggerTests.class);//强制类型转换了
+    @Test
+    public void testLogger(){
+        System.out.println(logger.getName());
+        logger.info("debug log");
+        logger.warning("warn log");
+    }
+}
